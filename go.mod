7d2276453145e73ed286697bd643dd27ae064125
Edwin Cloud <ecloud412@gmail.com>
2019-02-12 16:06:25 -0800

    Added functionality

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/edwintcloud/goslackit
 
 require (
 	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/nlopes/slack v0.5.0
 	github.com/pkg/errors v0.8.1 // indirect
 )
--- slack/slack.go
+++ slack/slack.go
@@ -1,7 +1,12 @@
 package slack
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/nlopes/slack"
@@ -38,9 +43,10 @@ func RespondToEvents(slackClient *slack.RTM) {
 		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
 			botTagString := fmt.Sprintf("<@%s> ", slackClient.GetInfo().User.ID)
-			if !strings.Contains(ev.Msg.Text, botTagString) {
-				continue
-			}
+
+			// if strings.Contains(ev.Msg.Text, botTagString) {
+			// 	continue
+			// }
 			message := strings.Replace(ev.Msg.Text, botTagString, "", -1)
 
 			// TODO: Make your bot do more than respond to a help command. See notes below.
@@ -72,8 +78,51 @@ func sendHelp(slackClient *slack.RTM, message, slackChannel string) {
 // sendResponse is NOT unimplemented --- write code in the function body to complete!
 
 func sendResponse(slackClient *slack.RTM, message, slackChannel string) {
-	command := strings.ToLower(message)
-	println("[RECEIVED] sendResponse:", command)
+	args := strings.Split(message, " ")
+
+	switch command := strings.ToLower(args[0]); command {
+	case "say":
+		slackClient.SendMessage(slackClient.NewOutgoingMessage(strings.Join(args[1:], " "), slackChannel))
+	case "math":
+		var a, b int64
+		var err error
+
+		a, err = strconv.ParseInt(args[1], 10, 64)
+		if err != nil {
+			slackClient.SendMessage(slackClient.NewOutgoingMessage("silly human you didn't enter valid math", slackChannel))
+			break
+		}
+
+		b, err = strconv.ParseInt(args[3], 10, 64)
+		if err != nil {
+			slackClient.SendMessage(slackClient.NewOutgoingMessage("silly human you didn't enter valid math", slackChannel))
+			break
+		}
+
+		switch operator := args[2]; operator {
+		case "+":
+			slackClient.SendMessage(slackClient.NewOutgoingMessage(fmt.Sprintf("%s %s %s = %d", args[1], args[2], args[3], a+b), slackChannel))
+		case "-":
+			slackClient.SendMessage(slackClient.NewOutgoingMessage(fmt.Sprintf("%s %s %s = %d", args[1], args[2], args[3], a-b), slackChannel))
+		case "/":
+			if b == 0 {
+				slackClient.SendMessage(slackClient.NewOutgoingMessage("nooooooo", slackChannel))
+				break
+			}
+			slackClient.SendMessage(slackClient.NewOutgoingMessage(fmt.Sprintf("%s %s %s = %d", args[1], args[2], args[3], a/b), slackChannel))
+		case "*":
+			slackClient.SendMessage(slackClient.NewOutgoingMessage(fmt.Sprintf("%s %s %s = %d", args[1], args[2], args[3], a*b), slackChannel))
+		default:
+			slackClient.SendMessage(slackClient.NewOutgoingMessage("silly human you didn't enter valid math", slackChannel))
+		}
+	case "xkcd":
+		slackClient.SendMessage(slackClient.NewOutgoingMessage(getImage(), slackChannel))
+		return
+	default:
+		slackClient.SendMessage(slackClient.NewOutgoingMessage("Silence Human", slackChannel))
+
+	}
+	println("[RECEIVED] sendResponse:", args[0])
 
 	// START SLACKBOT CUSTOM CODE
 	// ===============================================================
@@ -83,4 +132,28 @@ func sendResponse(slackClient *slack.RTM, message, slackChannel string) {
 	//      2. STRETCH: Write a goroutine that calls an external API based on the data received in this function.
 	// ===============================================================
 	// END SLACKBOT CUSTOM CODE
+}
+
+// get makes an api request to get weather for a location
+func getImage() string {
+	i := rand.Intn(1999)
+
+	// make request
+	res, _ := http.Get(fmt.Sprintf("https://xkcd.com/%d/info.0.json", i+1))
+
+	// read all into body
+	body, _ := ioutil.ReadAll(res.Body)
+
+	// declare struct
+	type response struct {
+		Image string `json:"img"`
+	}
+	data := response{}
+
+	// marshal into struct
+	json.Unmarshal(body, &data)
+
+	// return image url
+	return data.Image
+
 }
